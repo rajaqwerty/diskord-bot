e3894de15f96a66186f5d5c6c8976b4f75c720a2
rajaasyam <138883145+rajaqwerty@users.noreply.github.com>
2023-12-20 20:49:48 +0800

    Create pencegah-polusi.py

---
+++ pencegah-polusi.py
@@ -0,0 +1,130 @@
+# test-bot(bot class)
+# This example requires the 'members' and 'message_content' privileged intents to function.
+import requests
+import os
+import discord
+import random
+from discord.ext import commands
+from bot_logic import gen_pass
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+# command prefix 
+bot = commands.Bot(command_prefix='$', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+# adding two numbers
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+# spamming word
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+        
+# password generator        
+@bot.command()
+async def pw(ctx):
+    await ctx.send(f'Kata sandi yang dihasilkan: {gen_pass(10)}')
+
+# coinflip
+@bot.command()
+async def coinflip(ctx):
+    num = random.randint(1,2)
+    if num == 1:
+        await ctx.send('It is Head!')
+    if num == 2:
+        await ctx.send('It is Tail!')
+
+# rolling dice
+@bot.command()
+async def dice(ctx):
+    nums = random.randint(1,6)
+    if nums == 1:
+        await ctx.send('It is 1!')
+    elif nums == 2:
+        await ctx.send('It is 2!')
+    elif nums == 3:
+        await ctx.send('It is 3!')
+    elif nums == 4:
+        await ctx.send('It is 4!')
+    elif nums == 5:
+        await ctx.send('It is 5!')
+    elif nums == 6:
+        await ctx.send('It is 6!')
+
+# give local meme
+@bot.command()
+async def meme(ctx):
+    # try by your self 2 min
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+ 
+    await ctx.send(file=picture)
+
+# duck and dog API
+def get_dog_image_url():
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+@bot.command('dog')
+async def dog(ctx):
+    '''Setiap kali permintaan dog (anjing) dipanggil, program memanggil fungsi get_dog_image_url'''
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
+
+def get_duck_image_url():
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+@bot.command('duck')
+async def duck(ctx):
+    '''Setiap kali permintaan duck (bebek) dipanggil, program memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def tulis(ctx, *, my_string: str):
+    with open('kalimat.txt', 'w', encoding='utf-8') as t:
+        text = ""
+        text += my_string
+        t.write(text)
+
+@bot.command()
+async def tambahkan(ctx, *, my_string: str):
+    with open('kalimat.txt', 'a', encoding='utf-8') as t:
+        text = "\n"
+        text += my_string
+        t.write(text)
+
+@bot.command()
+async def baca(ctx):
+    with open('kalimat.txt', 'r', encoding='utf-8') as t:
+        document = t.read()
+        await ctx.send(document)
+
+# welcome message
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run('')
